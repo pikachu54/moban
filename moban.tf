3ade2ec5e2921726fbf725568d3058e55d50b7c8
pikachu54 <77178208+pikachu54@users.noreply.github.com>
2022-06-14 16:47:04 +0800

    Update moban.tf

--- moban.tf
+++ moban.tf
@@ -14,11 +14,6 @@ resource "ansible_host" "salt-proxy" {
     proxy_docker_tag          = var.proxy_docker_tag
   }
 }
-provider "alicloud" {
-        access_key = "LTAI5tELihjv9s88iz8xd5g8"
-        secret_key = "amvPoLc2PTnxLqfoUU7EGT2QenAtcr"
-        region = "cn-hangzhou"
-}
-                                                                                                                                                                                        
+                                                                                                                                                                                   
                                                                                                                                                                                                      
 
