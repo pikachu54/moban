13931b2fc31b78825b59f595081cb3cc49198ed4
pikachu54 <77178208+pikachu54@users.noreply.github.com>
2022-06-15 16:58:01 +0800

    Create main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,5 @@
-provider "aws" {
-  region  = "us-east-1"
-}
-
-provider "random" {}
-
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 2.0"
-    } 
-    random = {
-      source = "hashicorp/random"
-      version = "~> 3.1.0"
-    } 
-  }
+provider "alicloud" {
+        access_key = "LTAI5tELihjv9s88iz8xd5g8"
+        secret_key = "amvPoLc2PTnxLqfoUU7EGT2QenAtcr"
+        region = "cn-hangzhou"
 }
